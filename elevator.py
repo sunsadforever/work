fa6b9f68c43294cd5b757b464278ee8190f11152
sunsadforever <sunsad11314167@gmail.com>
2025-11-09 17:07:43 +0800

    Refactor elevator logic and class structure
    Refactor elevator class and logic for improved functionality.

--- elevator.py
+++ elevator.py
@@ -1,83 +1,72 @@
-import random
 import time
-building=[str(floor) for floor in range(random.randint(-6,-1),random.randint(7,15))]#有幾層樓
-building.remove('0')
-#print(building)
-for i in range(len(building)):
-    if '-' in building[i]:
-        building[i]=building[i].replace('-','B')
-print(*building)
-class elevator():
+import random
+class elevators:
     def __init__(self,floor,name):
         self.floor=floor
-        self.name=f'第{name}部電梯'
-elevator_list=[elevator(random.choice(building),i) for i in range(1,random.randint(1,3)+1)] #有幾個電梯
-print(f'你所在的大樓有{len(building)}層')
-print(f'本大樓有{len(elevator_list)}部電梯')
-now_location=0
-while True:
-    now_location=input('你在哪一層樓:')
-    if now_location in building:
-        break
-if now_location ==building[len(building)-1]:
-    button=['向下']
-elif now_location==building[0]:
-    button=['向上']
+        self.name=f'第{name+1}部電梯'
+    def floor_distance(self,distance,real_distance): #distance是絕對值，單純計算我目前樓層跟電梯樓層的距離，而real_distance則沒有絕對值，因為要計算是上還是下
+        self.distance=distance
+        self.real_distance=real_distance
+bottom=random.randint(-6,0)
+top=random.randint(2,11)
+building_floor=[str(i) for i in range(bottom,top)]
+building_floor.remove('0')
+for floor in range(len(building_floor)):
+    if '-' in building_floor[floor]:
+        building_floor[floor]=building_floor[floor].replace('-','B')
+elevator=[elevators(random.choice(building_floor),i) for i in range(random.randint(1,3))]
+print('目前這棟大樓存在',*building_floor)
+now_floor=input('你目前的樓層: ')
+if now_floor !=building_floor[0] and now_floor!= building_floor[-1]:
+    up_or_down=input('你要上還是下: ')
+elif now_floor ==building_floor[0]:
+    up_or_down='上'
 else:
-    button=['向上','向下']
+    up_or_down='下'
+floor_index=building_floor.index(now_floor) #目前樓層在大樓樓層中的索引直
+for elevator_e in elevator:
+    elevator_floor=building_floor.index(elevator_e.floor)
+    elevator_e.floor_distance(abs(elevator_floor-floor_index),elevator_floor-floor_index)
+elevator_distance=[i.distance for i in elevator]
+#print(elevator_distance)
+min_distance=len(building_floor)+1
+wait=True
+for floor in elevator:
+    if floor.distance<min_distance:
+        min_distance=floor.distance
+        elevator_selected=floor
+        if floor.distance==0:
+            wait=False
+            break
+if wait:
+    if elevator_selected.real_distance>0: #大於0代表電梯在目前樓層的上面
+        for i in range(min_distance):
+            time.sleep(1)
+            elevator_selected.floor=building_floor[building_floor.index(elevator_selected.floor)-1]
+            elevator_selected.distance-=1
+            elevator_selected.real_distance-=1
+            print(f'{elevator_selected.name} 到{elevator_selected.floor}')
 
-next_floor=''
-while True:
-    answer=input('你要向下還是向上: ')
-    if answer in button:
-        next_floor=input('你要去哪個樓層: ')
-        if next_floor in building:
-            if answer =='向上' and building.index(next_floor)>building.index(now_location):
-                break
-            elif answer =='向下' and building.index(next_floor)<building.index(now_location):
-                break
-distance=[] #要比較每個電梯的距離
-'''for num in elevator_list:
-    print(num.name,':',num.floor)'''
-for num in range(len(elevator_list)):
-    distance.append(building.index(elevator_list[num].floor)-building.index(now_location))
-    #print(building.index(elevator_list[num].floor) - building.index(now_location))
-#print(distance)
-closest = min(distance, key=abs) #最小距離
-elevator_execute=0 #被選中的電梯的索引
-elevator_selected='' #被選中的電梯
-for num in range(len(distance)):
-    if abs(distance[num])==abs(closest):
-        elevator_execute=building.index(elevator_list[num].floor)
-        elevator_selected=elevator_list[num].name
-        #print('this: ',elevator_execute,'and',building.index(now_location),'distance',elevator_execute-building.index(now_location))
-        if distance[num]>0:                                             #如果電梯在我目前的樓層之上，distance[num]=building.index(elevator_list[num].floor)-building.index(now_location)
-            for up in range(abs(elevator_execute-building.index(now_location))):
-                elevator_execute -=1
-                elevator_list[num].floor=building[elevator_execute]
-                time.sleep(3)
-                print(f'{elevator_selected}目前在{elevator_list[num].floor}')
-        else:                                                           #如果電梯在我目前的樓層之下
-            for down in range(abs(elevator_execute-building.index(now_location))):
-                elevator_execute += 1
-                elevator_list[num].floor=building[elevator_execute]
-                time.sleep(3)
-                print(f'{elevator_selected}目前在{elevator_list[num].floor}')
-        print(f'{elevator_selected}打開')
+    else:
+        for i in range(min_distance):
+            time.sleep(1)
+            elevator_selected.floor=building_floor[building_floor.index(elevator_selected.floor)+1]
+            elevator_selected.distance+=1
+            elevator_selected.real_distance+=1
+            print(f'{elevator_selected.name} 到{elevator_selected.floor}')
+print('電梯門開了')
+goal_floor=input('你要去哪一層樓: ')
+goal_floor_index=building_floor.index(goal_floor)
+if goal_floor_index>floor_index:
+    for i in range(abs(goal_floor_index-floor_index)):
         time.sleep(1)
-        print(f'{elevator_selected}已關')
-        if building.index(next_floor)-building.index(now_location) < 0: #如果目的地樓層在目前樓層之下
-            for up in range(abs(building.index(now_location)-building.index(next_floor)) ):
-                elevator_execute -=1
-                elevator_list[num].floor = building[elevator_execute]
-                time.sleep(3)
-                print(f'{elevator_selected}目前在{elevator_list[num].floor}')
-        else:                                                           #如果目的地樓層在目前樓層之上
-            for down in range(abs(building.index(now_location)-building.index(next_floor)) ):
-                elevator_execute+=1
-                elevator_list[num].floor = building[elevator_execute]
-                time.sleep(3)
-                print(f'{elevator_selected}目前在{elevator_list[num].floor}')
-        print(f'{elevator_selected}打開')
-        now_location=next_floor
-        break
+        elevator_selected.floor = building_floor[building_floor.index(elevator_selected.floor) + 1]
+        print(f'{elevator_selected.name} 到{elevator_selected.floor}')
+else:
+    for i in range(abs(goal_floor_index-floor_index)):
+        time.sleep(1)
+        elevator_selected.floor=building_floor[building_floor.index(elevator_selected.floor)-1]
+        print(f'{elevator_selected.name} 到{elevator_selected.floor}')
+print(f'電梯門開了')
+now_floor=goal_floor
+floor_index=goal_floor_index
